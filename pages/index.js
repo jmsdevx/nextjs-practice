a1ff42e9125920e99a3ea2a5d75a7f955e2b604f
James Sheehan <jmsdevx@gmail.com>
2019-01-31 13:48:41 -0600

    dynamic routing and route masking

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
 import Layout from "../components/AppLayout";
+import Link from "next/link";
+
+const PostLink = props => (
+  <li>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+
 export default () => (
   <Layout>
-    <h1>Home Page</h1>
+    <h1>Blog</h1>
+    <ul>
+      <PostLink id="hello-nextjs" title="Hello Next.js" />
+      <PostLink id="learn-nextjs" title="Learn Next.js" />
+      <PostLink id="deploy-nextjs" title="Deploy Apps with Zeit" />
+    </ul>
   </Layout>
 );
